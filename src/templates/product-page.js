74655148f3ff10b055163e808dd4a64cbdea37d0
Savannah Herrera <skyhtunes@gmail.com>
2021-04-21 15:22:35 -0500

    Starting to fix indiv. product pages

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,11 +1,29 @@
 import * as React from "react"
 import MainTemplate from "../layouts/mainTemplate.js"
+// import ProductCategory from "../components/productCategory.js"
+// import { Link } from "gatsby"
+
 import "../index.css"
 
 export default function ProductPage({ pageContext }) {
     return (
         <>
-            <MainTemplate></MainTemplate>
+            <MainTemplate>
+                <h1>hellooo</h1>
+                <div className="col-md-6 col-sm-12">
+                    <div className="ourPlants">
+                        {/* <img src={plant.image.file.url} alt={plant.commonName} key={plant.id} className="resize img-fluid" />
+
+
+                        <h3 dangerouslySetInnerHTML={{ __html: plant.commonName }} className="product-title"></h3>
+                        <p dangerouslySetInnerHTML={{ __html: plant.sciName }} className="product-description"></p>
+                        <p dangerouslySetInnerHTML={{ __html: plant.light }} className="product-description"></p>
+                        <p dangerouslySetInnerHTML={{ __html: plant.price }} className="price"></p>
+                        <p className="link-button"><Link to={"/products/" + plant.id} className="add-to-cart">Add to Cart</Link> <Link to="/wishlist"><span role="img" aria-label="sparkles emoji" className="add-to-wishlist">✨ </span>Wishlist</Link></p> */}
+                        <p>hellooooo</p>
+                    </div>
+                </div>
+            </MainTemplate>
         </>
     )
 }
